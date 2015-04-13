a33a9e9ad2d5073de16d65c835e4fb5b2d4d77a6
massud <massudgnan@gmail.com>
2015-04-13 16:52:49 +0100

    refactored code

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -9,7 +9,7 @@ describe('Javabuzz', function() {
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+      expect(javabuzz._isDivisibleBy(3,3)).toBe(true);
     });
 
   });
@@ -17,14 +17,14 @@ describe('Javabuzz', function() {
   describe('knows when a number is NOT', function(){
 
     it('divisible by 3', function(){
-      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+      expect(javabuzz._isDivisibleBy(1,3)).toBe(false);
     });
 
   });
   describe('knows when a number is', function(){
 
     it('divisible by 5', function() {
-      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+      expect(javabuzz._isDivisibleBy(5,5)).toBe(true);
     });
 
   });
@@ -32,7 +32,7 @@ describe('Javabuzz', function() {
   describe('knows when a number is NOT', function(){
 
     it('divisible by 5', function(){
-      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+      expect(javabuzz._isDivisibleBy(1,5)).toBe(false);
     });
 
   });
@@ -40,7 +40,7 @@ describe('Javabuzz', function() {
   describe('knows when a number is', function(){
 
     it('divisible by 15', function(){
-      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
+      expect(javabuzz._isDivisibleBy(15,15)).toBe(true);
     });
 
   });
@@ -48,7 +48,7 @@ describe('Javabuzz', function() {
   describe('knows when a number is NOT', function(){
 
     it('divisible by 15', function(){
-      expect(javabuzz.isDivisibleByFifteen(16)).toBe(false);
+      expect(javabuzz._isDivisibleBy(16,15)).toBe(false);
     });
 
   });
