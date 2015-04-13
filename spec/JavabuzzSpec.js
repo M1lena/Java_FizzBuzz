a23fe473bd2a68da8b771065e033570390def8f1
massud <massudgnan@gmail.com>
2015-04-13 15:40:33 +0100

    divisible by 5 tests

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -11,4 +11,27 @@ describe('Javabuzz', function() {
 
   });
 
+  describe('knows when a number is NOT', function(){
+
+    it('divisible by 3', function(){
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+  });
+  describe('knows when a number is', function(){
+
+    it('divisible by 5', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT', function(){
+
+    it('divisible by 5', function(){
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+    });
+  });
 });
